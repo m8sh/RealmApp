f8a198705342a6a926954b229f126b4ad541de3e
m8sh <vanish25th@gmail.com>
2026-02-10 09:30:08 -0500

    embed change

--- app/games/imposter/page.js
+++ app/games/imposter/page.js
@@ -1,6 +1,15 @@
 export const metadata = {
   title: 'Realm App',
   description: 'Realm GC imposter game.',
+  openGraph: {
+    title: 'Realm App',
+    description: 'Realm GC imposter game.',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Realm App',
+    description: 'Realm GC imposter game.',
+  },
 };
 
 export { default } from './startScreen.js';
